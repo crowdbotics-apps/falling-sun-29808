933b301db3c8b11c8c4ba91312a1e7ebc1308a17
Crowdbotics <team@crowdbotics.com>
2021-08-14 14:10:36 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "falling_sun_29808Identifier",
+  url: "https://falling_sun_29808Identifier.botics.co",
+  api: "https://falling_sun_29808Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
